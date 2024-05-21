2c49de18984e74f1fd375fa7df4d57a3d9994b8b
Riperntw <hpalomor2000@alumno.ipn.mx>
2024-05-20 23:01:21 -0600

    Cambiospequeñoschi

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var con = mysql.createConnection({
     user: process.env.DB_USER,//'ujuxzmolakxbdlt0',  //DB_USER, ujuxzmolakxbdlt0
     password: process.env.DB_PASSWORD,//'OXdfMT6ZJt8jZQk8jBLk', //DB_PASSWORD, OXdfMT6ZJt8jZQk8jBLk
     database: process.env.DB_NAME,//'buvmysjovxerblls4fap', //DB_NAME, buvmysjovxerblls4fap
-    port: process.env.DB_POST
+    port: process.env.DB_PORT
 })
 con.connect();
 
@@ -28,7 +28,7 @@ app.use(express.static('public'));
 
 app.post('/agregarCarro',(req,res)=>{
     let carro=req.body.Carro;
-    let modelo=req.body.Modelo
+    let modelo=req.body.Modelo;
     let precio= req.body.Precio;
     console.log(carro)
     console.log(modelo)
@@ -50,10 +50,8 @@ app.post('/borrarCarro', (req,res)=>{
     });
     
 });
-//
-//fun consultar
 
-
+//funcion consultar
 app.get('/obtenerCarro',(req,res)=>{
     
     con.query('select * from Carros', (err,respuesta, fields)=>{
@@ -67,12 +65,12 @@ app.get('/obtenerCarro',(req,res)=>{
                             <td>${carros.id}</td>
                             <td>${carros.nombre_coche}</td>
                             <td>${carros.modelo_coche}</td>
-                            <td>${carros.precio_coche}</td>  
+                            <td>${carros.precio_coche}</td> 
+                            <td><button>Actualizar</button</td>
+                            <td><input type="submit" value="Borrar"></td> 
                             
                             </tr>`;
             
-
-
         });
 
         return res.send(`<table border=1>
@@ -81,6 +79,8 @@ app.get('/obtenerCarro',(req,res)=>{
                     <th>Nombre</th>
                     <th>Modelo</th>
                     <th>Precio</th>
+                    <th>Actualizar</th>
+                    <th>Borrar</th>
                 <tr>
                 ${carrosHTML}
                 </table>
@@ -90,6 +90,11 @@ app.get('/obtenerCarro',(req,res)=>{
 
     });
 });
+
+//funcion para actualizar
+
+//funcion para borrar
+
 
 app.listen(3000,()=>{
 
--- config.js
+++ config.js
@@ -3,4 +3,4 @@ export const DB_NAME = process.env.DB_NAME || 'buvmysjovxerblls4fap'
 export const DB_HOST = process.env.DB_HOST || 'buvmysjovxerblls4fap-mysql.services.clever-cloud.com'
 export const DB_PASSWORD = process.env.BD_PASSWORD || 'OXdfMT6ZJt8jZQk8jBLk'
 export const DB_USER = process.env.DB_USER || 'ujuxzmolakxbdlt0'
-export const DB_PORT = process.env.DB_CONST || 3306
+export const DB_PORT = process.env.DB_PORT || 3306
